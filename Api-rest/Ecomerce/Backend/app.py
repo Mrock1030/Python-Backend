3583668f0f69a08968d21c030c9239c830e06390
Mrock <jkmilo1030@gmail.com>
2026-01-10 11:42:36 -0500

    Levantando el servidor flask

---
+++ Api-rest/Ecomerce/Backend/app.py
@@ -0,0 +1,16 @@
+#importamos de main la función create_app
+from main import create_app
+import os
+
+#llamamos nuevamnete a la aplicación app
+app = create_app()
+
+##llamamos app .context
+"""para acceder a la aplicación en cualquier 
+del sistema"""
+app.app_context().push()
+
+#para saber si al app se esta ejecutando
+##el debug tru hace que se actulice a cada rato
+if __name__=='__main__':
+    app.run(port=os.getenv("PORT"),debug=True)
