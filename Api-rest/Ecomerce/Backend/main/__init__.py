826ddae36836234dbbdffae233e1fed8868de2ac
Mrock1030 <jkmilo1030@gmail.com>
2026-01-13 14:56:22 -0500

    añadimos el env, creamos varibles de entorno y nos conectamos a la nueva bd creada

--- Api-rest/Ecomerce/Backend/main/__init__.py
+++ Api-rest/Ecomerce/Backend/main/__init__.py
@@ -1,6 +1,7 @@
 #importamos la librerira os
 import os 
 from flask import Flask
+import sqlite3
 ##importamos el dotenv para tomar las variables de entorno
 from dotenv import load_dotenv
 
@@ -8,14 +9,31 @@ from dotenv import load_dotenv
 
 ##importamos el modulo para crear la api-rest
 from flask_restful import  Api
+##Importamos el modulo para conectarme euna base de datos sql
+from flask_sqlalchemy import SQLAlchemy
 ##definimos lo importado
 api = Api()
+#instanciamos lo anteriormente importado SQLAlchemy
+db=SQLAlchemy()
 
 ## para levantar nuetra aplicación
 def create_app():
     app = Flask(__name__)
     ##llamamos a las variables de entorno
+    #cargamos las variables de entorno
     load_dotenv()
+    #configuración de la base de datos.db
+    PATH = os.getenv("DATABASE_PATH")
+    DB_NAME = os.getenv("DATABASE_NAME")
+    #comprobamos si existe la base de datos
+    if not os.path.exists(os.path.join(PATH,DB_NAME)):
+        os.makedirs(PATH, exist_ok=True)
+        connect_db =sqlite3.connect(os.path.join(PATH,DB_NAME))
+        
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+    app.config["SQLALCHEMY_DATABASE_URI"]=f"sqlite:///{os.path.join(PATH,DB_NAME)}"
+    db.init_app(app)
+    
     #definimos la ruta
     import main.resources as resources
     ##agregamos la url que va tener para ser obtenido
