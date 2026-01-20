25f18b50ccbd663567e000541a2469a3e972ccd1
Mrock1030 <jkmilo1030@gmail.com>
2026-01-20 16:37:51 -0500

    Corrección de otros bugs

--- Api-rest/Ecomerce/Backend/main/resources/Productos.py
+++ Api-rest/Ecomerce/Backend/main/resources/Productos.py
@@ -14,19 +14,21 @@ class Producto(Resource):
     
     def put (self,id):
         producto = db.session.query(ProductoModel).get_or_404(id)
+        data = request.get_json().items()
         for key, value in data:
             setattr(producto,key,value)
+            
         try:
             db.session.add(producto)
-            db.sesion.commit()
+            db.session.commit()
             return producto.to_json(),201
         except:
             return '',404
         
     def delete(self, id):
-        producto=db.sesion.query(ProductoModel).get_or_404(id)
+        producto=db.session.query(ProductoModel).get_or_404(id)
         try:
-            db.sesion.delete(producto)
+            db.session.delete(producto)
             db.session.commit()
         except:
             return '',404
