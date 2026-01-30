ed245139c6353b268dc6381a66040eeda1e98085
Mrock1030 <jkmilo1030@gmail.com>
2026-01-29 22:00:04 -0500

    Mejoras funcionales

--- Api-rest/Ecomerce/Backend/main/models/__init__.py
+++ Api-rest/Ecomerce/Backend/main/models/__init__.py
@@ -1,4 +1,4 @@
 from .Usuario import Usuario as UsuarioModel 
 from .Producto import Producto as ProductoModel
 from .Compra import Compra as CompraModel 
-from .ProductoCompra import ProductoCompra as ProdutoCompraModel      
+from .ProductoCompra import ProductoCompra as ProductoCompraModel      
--- Api-rest/Ecomerce/Backend/main/resources/ProductosCompras.py
+++ Api-rest/Ecomerce/Backend/main/resources/ProductosCompras.py
@@ -2,17 +2,48 @@ from flask_restful import Resource
 #importamos jsonify para devolver todo en tipo json
 from flask import jsonify, request
 from main import db 
-from main.models import ProdutoCompraModel
+from main.models import ProductoCompraModel
 
 class ProductoCompra(Resource):
     def get (self):
-        productoCompras = db.session.query(ProdutoCompraModel).all()
+        productoCompras = db.session.query(ProductoCompraModel).all()
         return jsonify({
             "productoCompra":[productoCompra.to_json() for productoCompra in productoCompras ]
         })
-
+        
+    def post(self):
+        productocompra = ProductoCompraModel.from_json(request.get_json())
+        db.session.add(productocompra)
+        db.session.commit()
+        return productocompra.to_json(), 201
+    
 class ProductosCompras(Resource):
-        def post(self):
-        productocompra= ProdutoCompraModel.from_json(request.get_json())
-        db.session.add(productocompra)        db.session.commit()
-        return compra.to_json(),201    
+    def get(self,id):
+        productocompra = db.sesion.query(ProductoCompraModel).get_or_404(id)
+        try:
+            return  productocompra.to_json()
+        except:
+            return '' , 404
+        
+    def delete(self,id).
+        productocompra = db.session.query(ProductoCompraModel).get_or_404(id)
+        try:
+            db.session.delete(productocompra)
+            db.session.commit()
+            return '', 204
+        except:
+            return '', 404
+        
+    def put(self,id):
+        productocompra = db.session.query(ProductoCompraModel).get_or_404(id)
+        data = request.get_json().items()
+        for i value in data:
+            setattr(productocompra, i ,value)
+        try:
+            db.session.add(productocompra)
+            db.session.commit()
+            return productocompra.to_json(),201
+        except:
+            return '',404
+        
+            
